af6002b52000932fde358b4086de449964bdb2f1
jaden-hicks <jaden.hicks@umusic.com>
2019-06-12 14:59:29 -0500

    add routes

--- routes/user.js
+++ routes/user.js
@@ -1,51 +1,41 @@
-import axios from 'axios'
+import request from 'request'
 
-const clientID = '85ec7eb9dc0543fc9408c8ba05fd2bdb';
-const clientSecret = 'c9192d5af4bb450da0770bf5b23f4e49';
-const redirect_uri = 'http://localhost:3000/user'
+module.exports.user = (app) => {
 
+    app.get('/userProfile', (req, res) => {
+        const access_token = req.session.access_token
 
-export function getUserAccess(app){
-    app.get('/test', (req, res) => {
-        return axios.get('https://accounts.spotify.com/authorize', {
-            params: {
-                client_id: clientID,
-                response_type: 'code',
-                redirect_uri,
-                // todo add state (refer to guide) here for security. Highly recommended
-            }
-        }).then(response => {
-            console.log(response);
+        const options = {
+            url: 'https://api.spotify.com/v1/me',
+            headers: {'Authorization': 'Bearer ' + access_token},
+            json: true
+        };
 
-            app.get('/user', (req, res) =>{
-                return axios({
-                    method: 'POST',
-                    url: 'https://accounts.spotify.com/api/token',
-                    params: {
-                        grant_type: "authorization_code",
-                        code: req.query.code,
-                        redirect_uri,
-                    },
-                    headers: {
-                        'Authorization': 'Basic ' + (new Buffer(clientID + ':' + clientSecret).toString('base64'))
-                    }
-                }).then(response => {
-                    console.log(response.data.access_token);
-                    return response.data.access_token
-                }).catch(err => {
-                    console.log(err)
-                })
-            })
-
-        }).catch(err => {
-            console.log(err);
-        })
+        request.get(options, function (error, response, body) {
+            res.end(JSON.stringify(body, undefined, 2))
+        });
     })
 
+    //todo check to see if scope works
+    app.get('/top', (req, res) => {
+        const access_token = req.session.access_token
+        const type = 'tracks' //tracks or artists
+
+        const options = {
+            type: 'tracks',
+            url: 'https://api.spotify.com/v1/me/top/tracks',
+            headers: {'Authorization': 'Bearer ' + access_token},
+            time_range: 'short_term',
+            offset: 1,
+            limit: 5,
+            json: true
+        };
+
+        request.get(options, function (error, response, body) {
+            console.log(body)
+            res.end(JSON.stringify(body, undefined, 2))
+        });
+    })
+
+
 }
-
-
-
-
-
-
