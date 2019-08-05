5a7c55c5ee66e99fca0176268331b0134e68b516
jdnhix <jdhix98890@yahoo.com>
2019-08-05 11:47:13 -0500

    s3 bucket test

--- server.js
+++ server.js
@@ -94,6 +94,7 @@ initializeDatabases().then(db => {
   // Start the server
   const server = app.listen(port, async () => {
     const host = ip.address()
+    console.log(host)
     const {port} = server.address();
     console.log('Server Listening at http://%s:%s/api-docs', host, port);
   });
